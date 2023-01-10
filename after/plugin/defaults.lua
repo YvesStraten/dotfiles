c4b2157dc479eca7fd9cd6b5a13a9310218f2e82
Storm <yves.straten@gmail.com>
2023-01-10 08:54:24 +0800

    Disaster

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -6,19 +6,19 @@ vim.g.UltiSnipsJumpBackwardTrigger="<c-k>"
 
 vim.g.vim_markdown_folding_disabled = 1
 
-vim.g.vim_markdown_conceal = 0
-vim.g.vim_markdown_math = 1
+vim.go.vim_markdown_conceal = 0
+vim.go.vim_markdown_math = 1
 
-vim.g.vim_markdown_frontmatter = 1
-vim.g.vim_markdown_toml_frontmatter = 1
-vim.g.vim_markdown_json_frontmatter = 1
+vim.go.vim_markdown_frontmatter = 1
+vim.go.vim_markdown_toml_frontmatter = 1
+vim.go.vim_markdown_json_frontmatter = 1
 
 vim.g.tex_flavor = "latex"
 vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf\#src:@line@tex"
-vim.g.vimtex_quickfix_mode = 0
-vim.o.conceallevel = 1
+vim.go.vimtex_quickfix_mode = 0
+vim.go.conceallevel = 1
 vim.g.tex_conceal = 'abdmg'
 
-vim.o.spell = 1
-vim.o.spelllang = "en_gb"
+vim.go.spell = true
+vim.go.spelllang = "en_gb"
