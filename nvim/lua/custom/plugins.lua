aec5cfd166ca9e4cf11227934f2cedd203be9d49
Storm <yves.straten@gmail.com>
2023-01-21 10:22:02 +0800

    Change file structure 2

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,52 @@
+return function(use)
+  use({
+    "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup({})
+      end
+  })
+
+  use({ 
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function()
+      vim.g.mkdp_filetypes = { "markdown" } 
+  end,
+  ft = { "markdown" },
+  cmd = { "MarkdownPreview" },
+  requires = { "zhaozg/vim-diagram", "aklt/plantuml-syntax" },
+  })
+  
+  use({ "SirVer/ultisnips",
+  })
+
+  use({ "honza/vim-snippets",
+  })
+
+  use({ "godlygeek/tabular",
+  })
+
+  use({ "elzr/vim-json",
+  })
+
+  use({ "plasticboy/vim-markdown"
+  })
+
+  use({ "vim-pandoc/vim-pandoc-syntax"
+  })
+
+   
+  use({ "preservim/nerdtree"
+  })
+
+  use({ "lervag/vimtex"
+  })
+  
+  use({ "ckunte/latex-snippets-vim"
+  }) 
+
+  use({ "KeitaNakamura/tex-conceal.vim"
+  })
+  
+end
+
