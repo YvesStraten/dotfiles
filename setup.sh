86c9575552572ed8ee54736d7fdb93a6f9986089
Storm <yves.straten@gmail.com>
2023-02-11 15:13:07 +0800

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 rm -r "$HOME/.config/nvim"
 rm -r "$HOME/.config/alacritty"
 
-ln -s "$(pwd)/nvim"
+ln -s "$(pwd)/nvim" ~/.config/nvim
 
-ln -s "$(pwd)/alacritty"
+ln -s "$(pwd)/alacritty" ~/.config/alacritty
