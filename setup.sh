73040980b16c7116d805afa03ed0adb60994a096
Storm <yves.straten@gmail.com>
2023-02-15 09:06:11 +0800

    Changed setup accordingly

--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+# My own
 rm -r "$HOME/.config/nvim"
 rm -r "$HOME/.config/alacritty"
 
 ln -s "$(pwd)/nvim" ~/.config/nvim
-
 ln -s "$(pwd)/alacritty" ~/.config/alacritty
+
+#Other
+rm -r "$HOME/.config/dunst"
+rm -r "$HOME/.config/hypr"
+rm -r "$HOME/.config/kitty"
+rm -r "$HOME/.config/pipewire"
+rm -r "$HOME/.config/rofi"
+rm -r "$HOME/.config/swaylock"
+rm -r "$HOME/.config/waybar"
+rm -r "$HOME/.config/wlogout"
+
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/dunst" ~/.config/dunst
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/hypr" ~/.config/hypr
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/kitty" ~/.config/kitty
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/pipewire" ~/.config/pipewire 
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/rofi" ~/.config/rofi
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/swalock" ~/.config/swaylock
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/waybar" ~/.config/waybar
+ln -s "$(pwd)/submodules/hyprland-titus/dotconfig/wlogout" ~/.config/wlogout 
+
