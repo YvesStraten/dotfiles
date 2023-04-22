d30f5c41c422e6138a4e6ba07e020d913d88c780
Storm <yves.straten@gmail>
2023-04-22 08:31:26 +0800

    New begginings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,14 @@
-require("plugins-setup")
-require("core.options")
-require("core.keymaps")
-require("core.colorscheme")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- LSP
-require("plugins.lsp.mason")
-require("plugins.lsp.lspconfig")
-
--- Plugin imports
-require("plugins.nvim-cmp")
-require("plugins.comment")
-require("plugins.nvim-tree")
-require("plugins.lualine")
-require("plugins.gitsigns")
-
--- tex
-require("plugins.vimtex")
-
--- whichkey
-require("plugins.whichkey")
-
--- Ultisnips
-require("plugins.UltiSnips")
-
-require("plugins.wrapping")
+require("lazy").setup("plugins")
