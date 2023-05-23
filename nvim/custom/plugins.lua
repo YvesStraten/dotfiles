123080ebb45880d380c8b054f1da737292455301
storm <yves.straten@gmail.com>
2023-05-23 11:47:18 +0800

    Good, barebones start

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,4 +1,46 @@
 local plugins = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"c",
+				"cpp",
+			},
+		},
+	},
 
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"stylua",
+				"texlab",
+			},
+		},
+	},
+
+  {
+    "lervag/vimtex",
+    config = function ()
+      require("custom.configs.vimtex")
+    end
+  }
 }
 return plugins
