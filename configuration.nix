234ae96ed81b382650822cb7efada5b75e0e47d3
storm <yves.straten@gmail.com>
2023-05-27 16:58:36 +0800

    OSPROBER

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,10 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes"];
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
