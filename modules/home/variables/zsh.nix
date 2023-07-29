1dc6152aa3f689345e57f029982abea458cb57a5
yvess <yvess.straten@gmail.com>
2023-07-29 10:26:12 +0800

    Fixed neovide wayland

--- modules/home/variables/zsh.nix
+++ modules/home/variables/zsh.nix
@@ -5,10 +5,10 @@
     enableCompletion = true;
 
     shellAliases = {
-      # neovide = "WINIT_UNIX_BACKEND=x11 neovide";
-      neovide = "prime-run nixGLNvidia-535.54.03 neovide";
-      # updatenix = "nix flake update ~/Git-repos/dotfiles";
-      # upgradenix = "sudo nixos-rebuild switch --flake ~/Git-repos/dotfiles#nitro";
+      neovide = "env WINIT_UNIX_BACKEND=x11 prime-run nixGLNvidia-535.86.05 neovide";
+      # neovide = "prime-run nixGLNvidia-535.86.05 neovide";
+      updatenix = "nix flake update ~/Git-repos/dotfiles";
+      upgradenix = "sudo nixos-rebuild switch --flake ~/Git-repos/dotfiles#nitro";
       # updatesymlinks = "home-manager switch --flake ~/Git-repos/dotfiles";
       mux = "tmuxinator";
       nxdev = "nix develop -c $SHELL";
