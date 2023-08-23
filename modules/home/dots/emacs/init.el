8aab01c2b7e35e785dbf2d68b1b32c0e53cb2b07
storm <yves.straten@gmail.com>
2023-08-23 10:08:25 +0000

    Switched to straight el

--- modules/home/dots/emacs/init.el
+++ modules/home/dots/emacs/init.el
@@ -1,55 +1,20 @@
-(defvar elpaca-installer-version 0.5)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :files (:defaults (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo) (make-directory repo t)
-          (when (< emacs-major-version 28) (require 'subr-x))
-          (condition-case-unless-debug err
-              (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                       ((zerop (call-process "git" nil buffer t "clone"
-                                             (plist-get order :repo) repo)))
-                       ((zerop (call-process "git" nil buffer t "checkout"
-                                             (or (plist-get order :ref) "--"))))
-                       (emacs (concat invocation-directory invocation-name))
-                       ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                             "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                       ((require 'elpaca))
-                       ((elpaca-generate-autoloads "elpaca" repo)))
-                  (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-                (error "%s" (with-current-buffer buffer (buffer-string))))
-            ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 ;; Install use-package support
-(elpaca elpaca-use-package
-  ;; Enable :elpaca use-package keyword.
-  (elpaca-use-package-mode)
-  ;; Assume :elpaca t unless otherwise specified.
-  (setq elpaca-use-package-by-default t))
-
-;; Block until current queue processed.
-(elpaca-wait)
-
-;;When installing a package which modifies a form used at the top-level
-;;(e.g. a package which adds a use-package key word),
-;;use `elpaca-wait' to block until that package has been installed/configured.
-;;For example:
-;;(use-package general :demand t)
-;;(elpaca-wait)
-
 ;; Expands to: (elpaca evil (use-package evil :demand t))
 (use-package evil
   :init
@@ -68,14 +33,6 @@
   (evil-collection-init))
 
 (use-package evil-tutor)
-
-;;Turns off elpaca-use-package-mode current declartion
-;;Note this will cause the declaration to be interpreted immediately (not deferred).
-;;Useful for configuring built-in emacs features.
-(use-package emacs :elpaca nil :config (setq ring-bell-function #'ignore))
-
-;; Don't install anything. Defer execution of BODY
-(elpaca nil (message "Packages loaded"))
 
 (use-package exec-path-from-shell
   :config
@@ -172,7 +129,7 @@
   )
 
 (use-package doom-modeline
-  :ensure t
+  :straight t
   :init (doom-modeline-mode 1))
 
 (use-package nerd-icons
@@ -352,7 +309,7 @@ one, an error is signaled."
   :hook org-mode prog-mode)
 
 (use-package lsp-mode
-  :ensure t
+  :straight t
   :init
   (setq lsp-keymap-prefix "C-c l"
         lsp-log-io nil
@@ -419,7 +376,7 @@ one, an error is signaled."
 
 (use-package company 
   :init (global-company-mode)
-  :ensure t
+  :straight t
   :diminish company-mode
   :custom
   (company-idle-delay 0.0)
@@ -447,7 +404,7 @@ one, an error is signaled."
   )
 
 (use-package neotree
-  :ensure t
+  :straight t
   :config
   (ys/leader-keys
     "n" '(neotree-toggle :wk "Toggle neotree"))
@@ -510,7 +467,7 @@ one, an error is signaled."
     "fU" '(sudo-edit :wk "Sudo edit file")))
 
 (use-package dashboard
-:ensure t
+:straight t
 :init
 (setq initial-buffer-choice 'dashboard-open)
 (setq dashboard-set-heading-icons t)
@@ -520,7 +477,7 @@ one, an error is signaled."
 (dashboard-setup-startup-hook))
 
 (use-package catppuccin-theme 
-  :ensure t
+  :straight t
   :config
   (load-theme 'catppuccin t)
   (setq catppuccin-flavor 'mocha)
