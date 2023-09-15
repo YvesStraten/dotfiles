d6daab2fa6e1b02e406b7b5e4ca596b61f182038
yvess <yvess.straten@gmail.com>
2023-09-15 16:31:03 +0800

    Added dracula to emacs

--- home/emacs/alternate.el
+++ home/emacs/alternate.el
@@ -90,7 +90,7 @@
   (require 'smartparens-config)
   (smartparens-global-mode 1))
 
-(add-hook 'prog-mode-hook #'smartparens-mode)
+;; General.el
 
 ;; Adds magit
 (use-package magit
@@ -107,6 +107,31 @@
   :config
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
+;; Integrated term
+(use-package vterm)
+
+;; Which key
+(use-package which-key
+  :init
+  (which-key-mode 1)
+  :config
+  (setq which-key-side-window-location 'bottom
+        which-key-sort-order #'which-key-key-order-alpha
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10
+        which-key-side-window-max-height 0.25
+        which-key-idle-delay 0.8
+        which-key-max-description-length 25
+        which-key-allow-imprecise-window-fit t))
+
+;; All-the-icons
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+;; Disable unrelated warnings
 (setq warning-minimum-level :error)
 
 ;; Disable backup files (e.g., filename~)
@@ -116,10 +141,36 @@
 (setq auto-save-default nil)
 
 ;; Store all backup and auto-save files in a specific directory
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups/")))
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+(setq backup-directory-alist '(("." . "~/emacs/backups/")))
+(setq auto-save-file-name-transforms '((".*" "~/emacs/auto-save-list/" t)))
 
-;; Customize keybindings as needed
+;; Tabs and space
+(setq tab-width 2)
+(setq-default ident-tabs-mode nil)
+
+;; UI related things
+(use-package dracula-theme)
+(load-theme 'dracula t)
+
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono NF-15"))
+
+ (add-to-list 'default-frame-alist '(alpha-background . 80))
+
+ (defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (eql (cond ((numberp alpha) alpha)
+                    ((numberp (cdr alpha)) (cdr alpha))
+                    ;; Also handle undocumented (<active> <inactive>) form.
+                    ((numberp (cadr alpha)) (cadr alpha)))
+              100)
+         '(85 . 50) '(100 . 100)))))
+(global-set-key (kbd "C-c t") 'toggle-transparency)
+
+
+;; Keybinds 
 
 ;; End of init.el
 (custom-set-variables
@@ -127,8 +178,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 '("f25f174e4e3dbccfcb468b8123454b3c61ba94a7ae0a870905141b050ad94b8f" default))
  '(package-selected-packages
-   '(neotree company-lsp org-plus-contrib evil yasnippet-snippets vterm-toggle tree-sitter-langs magit all-the-icons lsp-ui lsp-treemacs ivy-yasnippet company)))
+	 '(which-key neotree company-lsp org-plus-contrib evil yasnippet-snippets vterm-toggle tree-sitter-langs magit all-the-icons lsp-ui lsp-treemacs ivy-yasnippet company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
