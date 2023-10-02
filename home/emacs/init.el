2b91221e70f00f29a8c8734a387e67d2fac9250f
yvess <yvess.straten@gmail.com>
2023-10-02 16:14:07 +0800

    Changed to Dracula doom theme

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -408,7 +408,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-tokyo-night t)
+  (load-theme 'doom-dracula t)
 
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config)
