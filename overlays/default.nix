ae7fa401e778d2b835607274b10ace63bf227f03
YvesStraten <yves.straten@gmail.com>
2023-12-21 15:34:02 +0800

    Switched to fetchTarball for emacs overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,65 +1,46 @@
-{ inputs
-, pkgs
-, ...
-}: {
+{ inputs, pkgs, ... }: {
   nixpkgs.overlays = [
     inputs.nur.overlay
 
-    inputs.emacs-overlay.overlay
-    (
-      final: prev: {
-        yvess =
-          (prev.yvess or { })
-          // (import ../packages/default.nix { inherit (prev) pkgs; });
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+      sha256 = "0nr8bpglvhjjkyh6xf091gb8nwqn66spycmhhzvkh5smahxpjw0n";
+    }))
 
-        nwg-displays = prev.nwg-displays.override {
-          hyprlandSupport = true;
+    (final: prev: {
+      yvess = (prev.yvess or { })
+        // (import ../packages/default.nix { inherit (prev) pkgs; });
+
+      nwg-displays = prev.nwg-displays.override { hyprlandSupport = true; };
+
+      sddm = prev.sddm.overrideAttrs (o: {
+        buildInputs = o.buildInputs
+          ++ [ final.qt5.qtquickcontrols2 final.qt5.qtgraphicaleffects ];
+      });
+
+      libsForQt5 = prev.libsForQt5 // {
+        sddm = prev.libsForQt5.sddm.overrideAttrs (o: {
+          buildInputs = o.buildInputs ++ [ final.qt5.qtgraphicaleffects ];
+        });
+      };
+
+      ani-cli-rofi = prev.ani-cli.overrideAttrs (o: rec {
+        desktop = pkgs.makeDesktopItem {
+          name = "ani-cli";
+          desktopName = "Anime cli";
+          comment = "A cli program to watch anime";
+          genericName = "Anime player";
+          categories = [ "Video" ];
+          exec = "ani-cli --rofi";
         };
 
-        sddm =
-          prev.sddm.overrideAttrs
-            (o: {
-              buildInputs =
-                o.buildInputs
-                ++ [
-                  final.qt5.qtquickcontrols2
-                  final.qt5.qtgraphicaleffects
-                ];
-            });
-
-        libsForQt5 =
-          prev.libsForQt5
-          // {
-            sddm =
-              prev.libsForQt5.sddm.overrideAttrs
-                (o: {
-                  buildInputs = o.buildInputs ++ [ final.qt5.qtgraphicaleffects ];
-                });
-          };
-
-        ani-cli-rofi =
-          prev.ani-cli.overrideAttrs
-            (
-              o: rec {
-                desktop =
-                  pkgs.makeDesktopItem
-                    {
-                      name = "ani-cli";
-                      desktopName = "Anime cli";
-                      comment = "A cli program to watch anime";
-                      genericName = "Anime player";
-                      categories = [ "Video" ];
-                      exec = "ani-cli --rofi";
-                    };
-
-                installPhase = ''
-                  mkdir -p $out/share/applications
-                  cp ${desktop}/share/applications/* $out/share/applications
-                  ${o.installPhase}
-                '';
-              }
-            );
-      }
-    )
+        installPhase = ''
+          mkdir -p $out/share/applications
+          cp ${desktop}/share/applications/* $out/share/applications
+          ${o.installPhase}
+        '';
+      });
+    })
   ];
 }
