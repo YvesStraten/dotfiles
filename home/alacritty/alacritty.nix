a00d9878297e4867546e979465d66eb9a0b21551
YvesStraten <yves.straten@gmail.com>
2023-12-27 15:08:08 +0800

    Fixed colors for alacritty

--- home/alacritty/alacritty.nix
+++ home/alacritty/alacritty.nix
@@ -1,7 +1,6 @@
-{
-  pkgs,
-  config,
-  ...
+{ pkgs
+, config
+, ...
 }: {
   programs.alacritty = {
     enable = true;
@@ -20,6 +19,30 @@
       };
 
       colors = with config.colorScheme.colors; {
+        bright = {
+          black = "0x${base00}";
+          blue = "0x${base0D}";
+          cyan = "0x${base0C}";
+          green = "0x${base0B}";
+          magenta = "0x${base0E}";
+          red = "0x${base08}";
+          white = "0x${base06}";
+          yellow = "0x${base09}";
+        };
+        cursor = {
+          cursor = "0x${base06}";
+          text = "0x${base06}";
+        };
+        normal = {
+          black = "0x${base00}";
+          blue = "0x${base0D}";
+          cyan = "0x${base0C}";
+          green = "0x${base0B}";
+          magenta = "0x${base0E}";
+          red = "0x${base08}";
+          white = "0x${base06}";
+          yellow = "0x${base0A}";
+        };
         primary = {
           background = "0x${base00}";
           foreground = "0x${base06}";
