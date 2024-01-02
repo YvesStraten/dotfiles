eafe11b3a94d7f24839aa730ac19e9ee0d438793
YvesStraten <yves.straten@gmail.com>
2024-01-02 18:17:31 +0800

    Added very useful kitty options

--- home/kitty/kitty.nix
+++ home/kitty/kitty.nix
@@ -4,6 +4,11 @@
     font.name = "JetBrainsMono Nerd Font";
     font.size = 23;
     shellIntegration.enableZshIntegration = true;
+    extraConfig = ''
+    macos_quit_when_last_window_closed yes
+    confirm_os_window_close 0
+    background_opacity 0.8
+    '';
     theme = "Gruvbox Dark";
   };
 }
