32060da8c02e8ffd8439e3c54fe5da3d5817b3bd
YvesStraten <yves.straten@gmail.com>
2024-02-02 09:00:03 +0800

    Had to change font cause of windows

--- home/emacs/config.el
+++ home/emacs/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 21 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 19))
+(setq doom-font (font-spec :family "JetBrainsMono NF" :size 21 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "JetBrainsMono NF" :size 19))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
