c8295d5bf38057c0368f8b3d77dde87a816291d3
YvesStraten <yves.straten@gmail.com>
2024-03-13 08:21:08 +0800

    Updated firefox

--- home/utils/firefox.nix
+++ home/utils/firefox.nix
@@ -1,16 +1,20 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: let
+{ pkgs
+, inputs
+, ...
+}:
+let
   firefox =
     if pkgs.stdenv.isDarwin
     then pkgs.firefox-bin
     else null;
-in {
+in
+{
   programs.firefox = {
     enable = true;
     package = firefox;
+    policies = {
+      AppAutoUpdate = false;
+    };
     profiles.yvess = {
       isDefault = true;
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -24,7 +28,10 @@ in {
         "layers.acceleration.force-enabled" = true;
         "gfx.webrender.all" = true;
         "svg.context-properties.content.enabled" = true;
+
         "app.update.auto" = false;
+        "app.update.silent" = true;
+        "browser.aboutConfig.showWarning" = false;
       };
       userChrome = ''
         ${builtins.readFile "${pkgs.yvess.theme}/userChrome.css"}
