5e05a4bb5d2fc24ed64ebdab8f1e231470cfe743
YvesStraten <yves.straten@gmail.com>
2024-04-03 02:17:00 +0000

    Fish managed by nix

--- home/dev/fish.nix
+++ home/dev/fish.nix
@@ -1,1 +1,36 @@
-{ pkgs, ... }: { programs.fish.enable = true; }
+{ pkgs, ... }: {
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ga = "git add";
+      gc = "git commit";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+    };
+
+    shellInit = ''
+      set -g fish_greeting
+    '';
+
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "tide";
+        src = tide.src;
+      }
+
+      {
+        name = "sponge";
+        src = sponge.src;
+      }
+    ] ++ (if pkgs.stdenv.isDarwin then [{
+      name = "nix-env.fish";
+      src = pkgs.fetchFromGitHub {
+        owner = "lilyball";
+        repo = "nix-env.fish";
+        rev = "master";
+        sha256 = "RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+      };
+    }] else
+      []);
+  };
+}
