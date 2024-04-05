7d7100425105f7c0b4924b9205eb08e061f672a2
YvesStraten <yves.straten@gmail.com>
2024-04-05 12:13:53 +0800

    Working jdtls without downloading again

--- home/dev/emacs/emacs.nix
+++ home/dev/emacs/emacs.nix
@@ -1,48 +1,40 @@
-{ pkgs
-, ...
-}:
+{ pkgs, ... }:
 let
   macport = pkgs.emacs-unstable.overrideAttrs (old: {
-    configureFlags = old.configureFlags ++ [
-      "--with-cairo"
-      "--with-json"
+    configureFlags = old.configureFlags ++ [ "--with-cairo" "--with-json" ];
+    patches = (old.patches or [ ]) ++ [
+      # Fix OS window role (needed for window managers like yabai)
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+        sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+      })
+      # Use poll instead of select to get file descriptors
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
+        sha256 = "jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
+      })
+      # Enable rounded window with no decoration
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+        sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+      })
+      # Make Emacs aware of OS-level light/dark mode
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+        sha256 = "oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
+      })
     ];
-    patches =
-      (old.patches or [ ])
-      ++ [
-        # Fix OS window role (needed for window managers like yabai)
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
-          sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
-        })
-        # Use poll instead of select to get file descriptors
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
-          sha256 = "jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
-        })
-        # Enable rounded window with no decoration
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
-          sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
-        })
-        # Make Emacs aware of OS-level light/dark mode
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
-          sha256 = "oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
-        })
-      ];
   });
 
-  emacs =
-    if pkgs.stdenv.isLinux
-    then pkgs.emacs-pgtk
-    else macport;
-in
-{
+  emacs = if pkgs.stdenv.isLinux then pkgs.emacs-pgtk else macport;
+in {
   programs.emacs = {
     enable = true;
-    package =
-      emacs;
+    package = emacs;
     extraPackages = epkgs:
       with epkgs; [
         vterm
@@ -53,41 +45,46 @@ in
       ];
   };
 
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    jq
+  home = {
+    sessionVariables = {
+      JDTLS_PATH = "${pkgs.jdt-language-server}/share/java/jdtls";
+    };
 
-    ispell
-    languagetool
-    nixd
-    texlab
-    sumneko-lua-language-server
-    stylua
-    nodePackages_latest.prettier
-    nodePackages_latest.vscode-html-languageserver-bin
-    nodePackages_latest.typescript-language-server
-    nodePackages_latest.eslint
-    python311Packages.pytest
-    python311Packages.pyflakes
-    python311Packages.debugpy
+    packages = with pkgs; [
+      ripgrep
+      fd
+      jq
 
-    html-tidy
-    shellcheck
-    nodePackages_latest.pyright
-    cppcheck
-    clang-tools
-    nixfmt
-    plantuml
-    black
+      ispell
+      languagetool
+      nixd
+      texlab
+      sumneko-lua-language-server
+      jdt-language-server
+      stylua
+      nodePackages_latest.prettier
+      nodePackages_latest.vscode-html-languageserver-bin
+      nodePackages_latest.typescript-language-server
+      nodePackages_latest.eslint
+      python311Packages.pytest
+      python311Packages.pyflakes
+      python311Packages.debugpy
 
-    shfmt
+      html-tidy
+      shellcheck
+      nodePackages_latest.pyright
+      cppcheck
+      clang-tools
+      nixfmt
+      plantuml
+      black
 
-    # DAP protocols
-    lldb
-  ];
+      shfmt
 
-  services.syncthing = {
-    enable = true;
+      # DAP protocols
+      lldb
+    ];
+
   };
+  services.syncthing = { enable = true; };
 }
