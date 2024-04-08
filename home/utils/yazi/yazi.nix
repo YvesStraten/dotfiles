e963925ebec5416b34ea8d612415db0a2b0557fe
YvesStraten <yves.straten@gmail.com>
2024-04-08 15:30:10 +0800

    Added new keymaps for `yazi`

--- home/utils/yazi/yazi.nix
+++ home/utils/yazi/yazi.nix
@@ -4,7 +4,7 @@
     enableZshIntegration = true;
   };
 
-  home.file.".config/yazi/yazi.toml" = {
-    source = ./config/yazi.toml;
-  };
+  home.file.".config/yazi/yazi.toml" = { source = ./config/yazi.toml; };
+
+  home.file.".config/yazi/keymap.toml" = { source = ./config/keymap.toml; };
 }
