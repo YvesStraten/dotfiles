02b97353b3df4c146d173401d46fe338f5372f01
YvesStraten <yves.straten@gmail.com>
2024-06-13 10:52:50 +0800

    Made overlay more fitting to nixpkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,10 @@
-{ inputs, pkgs, lib, ... }: {
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+{
   nixpkgs.overlays = [
     inputs.nur.overlay
     inputs.yazi.overlays.default
@@ -6,44 +12,49 @@
     inputs.emacs-overlay.overlay
 
     (final: prev: {
-      yvess = (prev.yvess or { })
-        // (import ../packages/default.nix { inherit (prev) pkgs; });
+      yvess = (prev.yvess or { }) // (import ../packages/default.nix { inherit (prev) pkgs; });
 
       jdt-language-server-wsl =
-        assert (lib.assertMsg (prev.jdt-language-server.version == "1.31.0" )
-          "Wsl? is this still needed?");
+        assert (lib.assertMsg (prev.jdt-language-server.version == "1.31.0") "Wsl? is this still needed?");
         pkgs.callPackage ../packages/jdt.nix { };
 
-      picom-pijulius = assert (lib.assertMsg (prev.picom-pijulius != null) "Now in nixpkgs");
-        pkgs.callPackage ../packages/picom-pijulius.nix {  };
+      picom-pijulius =
+        assert (lib.assertMsg (prev.picom-pijulius != null) "Now in nixpkgs");
+        pkgs.callPackage ../packages/picom-pijulius.nix { };
 
-      sddm = prev.sddm.overrideAttrs (o: {
-        buildInputs = o.buildInputs
-          ++ [ final.qt5.qtquickcontrols2 final.qt5.qtgraphicaleffects ];
+      sddm = prev.sddm.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ [
+          final.qt5.qtquickcontrols2
+          final.qt5.qtgraphicaleffects
+        ];
       });
 
       libsForQt5 = prev.libsForQt5 // {
-        sddm = prev.libsForQt5.sddm.overrideAttrs (o: {
-          buildInputs = o.buildInputs ++ [ final.qt5.qtgraphicaleffects ];
+        sddm = prev.libsForQt5.sddm.overrideAttrs (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs ++ [ final.qt5.qtgraphicaleffects ];
         });
       };
 
-      ani-cli-rofi = prev.ani-cli.overrideAttrs (o: rec {
-        desktop = pkgs.makeDesktopItem {
-          name = "ani-cli";
-          desktopName = "Anime cli";
-          comment = "A cli program to watch anime";
-          genericName = "Anime player";
-          categories = [ "Video" ];
-          exec = "ani-cli --rofi";
-        };
-
-        installPhase = ''
-          mkdir -p $out/share/applications
-          cp ${desktop}/share/applications/* $out/share/applications
-          ${o.installPhase}
-        '';
-      });
+      ani-cli-rofi = prev.ani-cli.overrideAttrs (
+        oldAttrs:
+        let
+          desktop = pkgs.makeDesktopItem {
+            name = "ani-cli";
+            desktopName = "Anime cli";
+            comment = "A cli program to watch anime";
+            genericName = "Anime player";
+            categories = [ "Video" ];
+            exec = "ani-cli --rofi";
+          };
+        in
+        {
+          installPhase = ''
+            mkdir -p $out/share/applications
+            cp ${desktop}/share/applications/* $out/share/applications
+            ${oldAttrs.installPhase}
+          '';
+        }
+      );
     })
   ];
 }
