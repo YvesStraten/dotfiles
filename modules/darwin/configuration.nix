ce641afdf894b6c3008f127fee8fc3ca4d24dea2
YvesStraten <yves.straten@gmail.com>
2024-11-25 11:03:49 +0800

    Replaced yabai with aerospace

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -31,37 +31,55 @@
     pkgs.vim
   ];
 
-  services.yabai = {
-    enable = true;
-    config = {
-      focus_follows_mouse = "autoraise";
-      mouse_follows_focus = "on";
-      window_placement = "second_child";
-      window_opacity = "off";
-      # top_padding = 10;
-      # bottom_padding = 10;
-      # left_padding = 10;
-      # right_padding = 10;
-      window_gap = 10;
-      layout = "bsp";
-    };
+	services.aerospace = {
+		enable = true;
+		settings = {
+			mode.main.binding = let 
+				workspaces = { i } : {
+					"alt-${builtins.toString i}" = "workspace " + builtins.toString i;
+				};
 
-    extraConfig = ''
-      yabai -m rule --add app='System Settings' manage=off
-      yabai -m rule --add app="Discord" manage=off
-      yabai -m rule --add app="WhatsApp" manage=off
-      yabai -m rule --add app="Finder" manage=off
-      yabai -m rule --add app="Thunderbird" manage=off
-      yabai -m rule --add app="IINA" manage=off
-      yabai -m rule --add title="yazi" manage=off
-    '';
-  };
+				keys = {
+					"h" = "left";
+					"j" = "down";
+					"k" = "up";
+					"l" = "right";
+				};
 
-  services.skhd = {
-    enable = true;
-    skhdConfig = "${builtins.readFile ./skhdrc}";
-  };
+			in {
+				alt-enter = "exec-and-forget open -na Kitty";
+				alt-b = "exec-and-forget open -na Firefox";
+			  alt-h = "focus left";
+			  alt-j = "focus down";
+			  alt-k = "focus up";
+			  alt-l = "focus right";
+				alt-shift-h = "move left";
+				alt-shift-j = "move down";
+				alt-shift-k = "move up";
+				alt-shift-l = "move right";
+				alt-1 = "workspace 1";
+				alt-2 = "workspace 2";
+				alt-3 = "workspace 3";
+				alt-4 = "workspace 4";
+				alt-5 = "workspace 5";
+				alt-6 = "workspace 6";
+				alt-7 = "workspace 7";
+				alt-8 = "workspace 8";
+				alt-9 = "workspace 9";
+				alt-shift-1 = "move-node-to-workspace 1";
+				alt-shift-2 = "move-node-to-workspace 2";
+				alt-shift-3 = "move-node-to-workspace 3";
+				alt-shift-4 = "move-node-to-workspace 4";
+				alt-shift-5 = "move-node-to-workspace 5";
+				alt-shift-6 = "move-node-to-workspace 6";
+				alt-shift-7 = "move-node-to-workspace 7";
+				alt-shift-8 = "move-node-to-workspace 8";
+				alt-shift-9 = "move-node-to-workspace 9";
+			};
+		};
+	};
 
+	system.stateVersion = 5;
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
