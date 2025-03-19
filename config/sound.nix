a26cbe5b677f6947abec9635a0e933b88b62055e
YvesStraten <yves.straten@gmail.com>
2025-03-19 09:10:47 +0800

    Power-save hack is no longer needed

--- config/sound.nix
+++ config/sound.nix
@@ -18,19 +18,7 @@ in
           alsa.enable = true;
           alsa.support32Bit = true;
           pulse.enable = true;
-
-          # disable camera to save battery
-          # https://reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
-          wireplumber = {
-            enable = true;
-            extraConfig = {
-              "10-disable-camera" = {
-                "wireplumber.profiles" = {
-                  main."monitor.libcamera" = "disabled";
-                };
-              };
-            };
-          };
+          wireplumber.enable = true;
         };
 
         pulseaudio.enable = false;
