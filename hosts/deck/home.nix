97c0d714daafffe2ecf6cc4a6e318ed1fdd44f56
YvesStraten <yves.straten@gmail.com>
2025-03-31 09:19:44 +0800

    Add dev utils because laptop is dead

--- hosts/deck/home.nix
+++ hosts/deck/home.nix
@@ -1,13 +1,38 @@
-{ pkgs, lib, ... }: {
-  custom.fish.enable = true;
-  custom.kitty.enable = true;
-  custom.firefox.enable = true;
-  custom.utils.enable = lib.mkForce false;
-  custom.general.enable = true;
-  custom.thunderbird.enable = lib.mkForce false;
-  custom.theming = {
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  custom = {
+    fish.enable = true;
+    kitty.enable = true;
+    firefox.enable = true;
+    utils.enable = lib.mkForce false;
+    emacs.enable = true;
+
+    general = {
+      enable = true;
+      extraPackages = with pkgs; [
+        libreoffice
+      ];
+    };
+
+    thunderbird.enable = lib.mkForce false;
+    theming = {
       enable = true;
       gtk.enable = false;
       qt.enable = false;
     };
+  };
+
+  services.emacs.enable = lib.mkForce false;
+
+  home.packages = [
+    pkgs.texlive.combined.scheme-medium
+    pkgs.wl-clipboard
+  ];
+
+  programs.direnv = {
+    enable = true;
+  };
 }
