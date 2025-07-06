b6c0c70f018840d203ebf968968133f4509591a1
YvesStraten <yves.straten@gmail.com>
2025-07-06 09:38:17 +0800

    Fixup hyprland config

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -1,9 +1,8 @@
-{
-  config,
-  options,
-  pkgs,
-  lib,
-  ...
+{ config
+, options
+, pkgs
+, lib
+, ...
 }:
 let
   cfg = config.custom.hyprland;
@@ -40,7 +39,7 @@ in
         rofi.enable = true;
         wlogout.enable = true;
         kanshi.enable = true;
-        nwg-dock.enable = true;
+        # nwg-dock.enable = true;
       };
 
       programs.hyprlock = {
@@ -92,28 +91,29 @@ in
               lock_cmd = "pidof hyprlock || hyprlock";
             };
 
-            listener = let
-              brightnessctl = lib.getExe pkgs.brightnessctl;
-            in [
-              {
-                timeout = 60;
-                on-timeout = "${brightnessctl} -s set 10";
-                on-resume = "${brightnessctl} -r";
-              }
+            listener =
+              let
+                brightnessctl = lib.getExe pkgs.brightnessctl;
+              in
+              [
+                {
+                  timeout = 60;
+                  on-timeout = "${brightnessctl} -s set 10";
+                  on-resume = "${brightnessctl} -r";
+                }
 
-              {
-                timeout = 90;
-                on-timeout = "loginctl lock-session";
-              }
+                {
+                  timeout = 90;
+                  on-timeout = "loginctl lock-session";
+                }
 
-              {
-                timeout = 300;
-                on-timeout = "hyprctl dispatch dpms off";
-                on-resume = "hyprctl dispatch dpms on";
-              }
-            ];
+                {
+                  timeout = 300;
+                  on-timeout = "hyprctl dispatch dpms off";
+                  on-resume = "hyprctl dispatch dpms on";
+                }
+              ];
           };
-
         };
       };
 
@@ -148,51 +148,50 @@ in
           in
           {
             "$mod" = "super";
-            bindl = ",switch:Lid Switch, exec, pidof hyprlock || hyprlock";
-            bind =
-              with pkgs;
-              let
-                screenshot = pkgs.writeShellScriptBin "screenshot" ''
-                  set -euxo pipefail
-                  ${grim}/bin/grim -g "$(${slurp}/bin/slurp)" - | ${swappy}/bin/swappy -f - -o "${config.xdg.userDirs.pictures}/Screenshots/swappy-$(date).png"
-                '';
-              in
-              [
-                "$mod, B, exec, uwsm app -- firefox"
-                "$mod, F1, exec, uwsm app -- ~/.config/hypr/scripts/keybind"
-                ", XF86AudioRaiseVolume, exec, uwsm app -- ${pamixer}/bin/pamixer -i 5"
-                ", XF86AudioLowerVolume, exec, uwsm app -- ${pamixer}/bin/pamixer -d 5"
-                ", XF86MonBrightnessUp, exec, uwsm app -- ${brightnessctl}/bin/brightnessctl s +5%"
-                ", XF86MonBrightnessDown, exec, uwsm app -- ${brightnessctl}/bin/brightnessctl s 5%-"
-                "SUPER SHIFT, x, exec, uwsm app -- ${hyprpicker}/bin/hyprpicker | ${wl-clipboard}/bin/wl-copy"
-                "$mod, C, exec, ${pamixer}/bin/pamixer -m && uwsm app -- pidof hyprlock || hyprlock && ${pamixer}/bin/pamixer -u"
-                "$mod, Return, exec, uwsm app -- ${kitty}/bin/kitty"
-                "$mod, E, exec, uwsm app -- emacsclient -c"
-                "$mod, N, exec, uwsm app -- yazi.desktop"
-                "$mod, R, exec, uwsm app -- rofi -show drun -run-command 'uwsm app -- {cmd}'"
-                "$mod, Q, killactive,"
-                "$mod, F, fullscreen,"
-                "$mod, Space, togglefloating,"
-                "$mod, I, pseudo, # dwindle"
-                "$mod, S, togglesplit, # dwindle"
-                "$mod CTRL, h, resizeactive, -20 0"
-                "$mod CTRL, l, resizeactive, 20 0"
-                "$mod CTRL, j, resizeactive, 0 -20"
-                "$mod CTRL, k, resizeactive, 0 20"
-                "SUPER ALT, up, workspace, e+1"
-                "SUPER ALT, down, workspace, e-1"
-                "$mod, g, togglegroup"
-                "$mod, tab, changegroupactive"
-                "$mod, grave, togglespecialworkspace"
-                "$mod SHIFT, grave, movetoworkspace, special"
-                "$mod, v, exec, uwsm app -- cliphist list | ${rofi}/bin/rofi -dmenu | cliphist decode | wl-copy"
-                "$mod SHIFT, s, exec, uwsm app -- ${screenshot}/bin/screenshot"
-              ]
-              ++ (
-                # workspaces
-                # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-                builtins.concatLists (
-                  builtins.genList (
+            # bindl = ",switch:Lid Switch, exec, pidof hyprlock || hyprlock";
+            bind = with pkgs; let
+              screenshot = pkgs.writeShellScriptBin "screenshot" ''
+                set -euxo pipefail
+                ${grim}/bin/grim -g "$(${slurp}/bin/slurp)" - | ${swappy}/bin/swappy -f - -o "${config.xdg.userDirs.pictures}/Screenshots/swappy-$(date).png"
+              '';
+            in
+            [
+              "$mod, B, exec, uwsm app -- firefox"
+              "$mod, F1, exec, uwsm app -- ~/.config/hypr/scripts/keybind"
+              ", XF86AudioRaiseVolume, exec, uwsm app -- ${pamixer}/bin/pamixer -i 5"
+              ", XF86AudioLowerVolume, exec, uwsm app -- ${pamixer}/bin/pamixer -d 5"
+              ", XF86MonBrightnessUp, exec, uwsm app -- ${brightnessctl}/bin/brightnessctl s +5%"
+              ", XF86MonBrightnessDown, exec, uwsm app -- ${brightnessctl}/bin/brightnessctl s 5%-"
+              "SUPER SHIFT, x, exec, uwsm app -- ${hyprpicker}/bin/hyprpicker | ${wl-clipboard}/bin/wl-copy"
+              "$mod, C, exec, ${pamixer}/bin/pamixer -m && uwsm app -- pidof hyprlock || hyprlock && ${pamixer}/bin/pamixer -u"
+              "$mod, Return, exec, uwsm app -- ${kitty}/bin/kitty"
+              "$mod, E, exec, uwsm app -- emacsclient -c"
+              "$mod, N, exec, uwsm app -- yazi.desktop"
+              "$mod, R, exec, uwsm app -- rofi -show drun -run-command 'uwsm app -- {cmd}'"
+              "$mod, Q, killactive,"
+              "$mod, F, fullscreen,"
+              "$mod, Space, togglefloating,"
+              "$mod, I, pseudo, # dwindle"
+              "$mod, S, togglesplit, # dwindle"
+              "$mod CTRL, h, resizeactive, -20 0"
+              "$mod CTRL, l, resizeactive, 20 0"
+              "$mod CTRL, j, resizeactive, 0 -20"
+              "$mod CTRL, k, resizeactive, 0 20"
+              "SUPER ALT, up, workspace, e+1"
+              "SUPER ALT, down, workspace, e-1"
+              "$mod, g, togglegroup"
+              "$mod, tab, changegroupactive"
+              "$mod, grave, togglespecialworkspace"
+              "$mod SHIFT, grave, movetoworkspace, special"
+              "$mod, v, exec, uwsm app -- cliphist list | ${rofi}/bin/rofi -dmenu | cliphist decode | wl-copy"
+              "$mod SHIFT, s, exec, uwsm app -- ${screenshot}/bin/screenshot"
+            ]
+            ++ (
+              # workspaces
+              # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+              builtins.concatLists (
+                builtins.genList
+                  (
                     i:
                     let
                       ws = i + 1;
@@ -201,25 +200,26 @@ in
                       "$mod, code:1${toString i}, workspace, ${toString ws}"
                       "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
                     ]
-                  ) 9
-                )
+                  )
+                  9
               )
-              ++
-
-                (builtins.concatLists (
-                  builtins.map (key_attr: [
-                    "$mod, ${key_attr.key}, movefocus, ${key_attr.direction} "
-                  ]) keys_directions
-                ))
-
-              ++ (
-
-                builtins.concatLists (
-                  builtins.map (key_attr: [
+            )
+            ++ (builtins.concatLists (
+              builtins.map
+                (key_attr: [
+                  "$mod, ${key_attr.key}, movefocus, ${key_attr.direction} "
+                ])
+                keys_directions
+            ))
+            ++ (
+              builtins.concatLists (
+                builtins.map
+                  (key_attr: [
                     "$mod SHIFT, ${key_attr.key}, movewindow, ${key_attr.direction} "
-                  ]) keys_directions
-                )
-              );
+                  ])
+                  keys_directions
+              )
+            );
 
             windowrulev2 = [
               "float, class:^(org.gnome.Nautilus)$"
@@ -229,13 +229,12 @@ in
               "idleinhibit focus, class:^(mpv|.+exe)$"
               "idleinhibit focus, class:^(firefox)$, title:^(.*YouTube.*)$"
               "idleinhibit fullscreen, class:^(firefox)$"
-
             ];
-
           };
 
         extraConfig = with pkgs; ''
-          exec-once = swww init
+          env = HYPRCURSOR_THEME,Bibata-Modern-Ice
+          env = HYPRCURSOR_SIZE,26
           monitor = ,highrr,auto,1
 
           input {
@@ -257,7 +256,7 @@ in
 
           bind = $mod, V, exec, uwsm app -- ${cliphist}/bin/cliphist list | wofi --show dmenu | ${cliphist}/bin/cliphist decode | ${wl-clipboard}/bin/wl-copy
           bind = $mod, escape, exec, uwsm app -- ${wlogout}/bin/wlogout --protocol layer-shell -b 5 -T 400 -B 400
-          bind = $mod, period, exec, uwsm app -- rofi -modi emoji -show emoji --action copy
+          bind = $mod, period, exec, uwsm app -- rofi -modi emoji -show emoji
 
           general {
                   gaps_in=10
