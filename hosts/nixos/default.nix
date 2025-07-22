a0f08a35ddc8771d03094f3ad0d5a6333ac906ab
YvesStraten <yves.straten@gmail.com>
2025-07-22 20:49:46 +0800

    Nvidia offload for vivobook

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -16,6 +16,20 @@
     design.enable = true;
   };
 
+  services.xserver.videoDrivers = [
+    "modesetting"
+  ];
+
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
   # Set your time zone.
   time.timeZone = "Asia/Makassar";
 
