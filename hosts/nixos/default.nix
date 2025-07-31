81a717eb9f0213da3717e479c709eca449ee63c7
YvesStraten <yves.straten@gmail.com>
2025-07-31 14:40:30 +0800

    Add more locales

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -33,19 +33,26 @@
   # Set your time zone.
   time.timeZone = "Asia/Makassar";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "de_DE.UTF-8";
+  i18n = {
+    # Select internationalisation properties.
+    defaultLocale = "de_DE.UTF-8";
+    supportedLocales =
+      let
+        genLocale = locale: "${locale}.UTF-8/UTF-8";
+      in
+      map genLocale [ "en_US" "de_DE" "sv_SE" ];
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
   };
 
   networking.hostId = "14b2792a";
