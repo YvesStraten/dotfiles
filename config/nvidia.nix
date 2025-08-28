8c90fdd8e33d9ae098874f5d16ba7a2c63aded18
YvesStraten <yves.straten@gmail.com>
2025-08-28 16:26:12 +0200

    Use open nvidia drivers

--- config/nvidia.nix
+++ config/nvidia.nix
@@ -23,7 +23,7 @@ in
       nvidia = {
         package = config.boot.kernelPackages.nvidiaPackages.latest;
         modesetting.enable = true;
-        open = false;
+        open = true;
       };
     };
   };
