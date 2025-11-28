27e253a47c5f1ad90ec9bf8835c7cd231374399a
YvesStraten <yves.straten@gmail.com>
2025-11-28 15:56:18 +0100

    Add polkit gnome

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -79,7 +79,26 @@ in
         };
       };
 
-      systemd.user.services.poweralertd.Unit.After = lib.mkForce [ "graphical-session.target" ];
+      systemd.user.services = {
+        poweralertd.Unit.After = lib.mkForce [ "graphical-session.target" ];
+        polkit-gnome-authentication-agent-1 = {
+          Unit = {
+            Description = "polkit-gnome-authentication-agent-1";
+            Wants = [ "graphical-session.target" ];
+            After = [ "graphical-session.target" ];
+          };
+          Install = {
+            WantedBy = [ "graphical-session.target" ];
+          };
+          Service = {
+            Type = "simple";
+            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+        };
+      };
 
       services = {
         cliphist = {
