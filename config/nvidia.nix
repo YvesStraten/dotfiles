8eb126dd9b06743c152bcd788eab1156ded7e68b
YvesStraten <yves.straten@gmail.com>
2025-12-09 08:14:01 +0100

    Use powermanagement from nvidia

--- config/nvidia.nix
+++ config/nvidia.nix
@@ -31,6 +31,9 @@ in
         package = config.boot.kernelPackages.nvidiaPackages.latest;
         modesetting.enable = true;
         open = true;
+        powerManagement.enable = true;
+        powerManagement.finegrained = true;
+        dynamicBoost.enable = true;
       };
     };
   };
